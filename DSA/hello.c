24ff801ba5b4ec9162b38b8d64a7697930f870af
root <root@localhost.localdomain>
2025-01-15 12:19:52 +0530

    Added source code

--- DSA/hello.c
+++ DSA/hello.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Print a message to the console
-    printf("Hello, World!\nWelcome to the world of C programming...");
+    printf("Hello, World!\nWelcome to the world of C programming...\n");
 
     // Return 0 to indicate successful execution
     return 0;
