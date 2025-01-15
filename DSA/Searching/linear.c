3b4c3e29df41fb32120de659bcc7105b5619f2da
Omii1908 <omi844198@gmail.com>
2025-01-15 18:52:38 +0530

    added folder containing searching algo

---
+++ DSA/Searching/linear.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, element, found = 0;
+
+    // Input array size
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input the element to search
+    printf("Enter the element to search: ");
+    scanf("%d", &element);
+
+    // Perform linear search
+    for (i = 0; i < n; i++) {
+        if (arr[i] == element) {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
+}
