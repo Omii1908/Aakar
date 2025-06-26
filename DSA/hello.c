dbadf1651d7534a0bc58a8fd91c2ebcdafe573b3
Omii1908 <Omi844198@gmail.com>
2025-06-26 10:22:01 +0530

    created a new branch for JAVA

--- DSA/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Print a message to the console
-    printf("Hello, World!\nWelcome to the world of C programming...\n");
-
-    // Return 0 to indicate successful execution
-    return 0;
-}
